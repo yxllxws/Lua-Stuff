9dd088549024a32e36a571ba33872d70911b52e4
Yxllxws <100077736+yxllxws@users.noreply.github.com>
2023-11-24 21:55:07 +0000

    Update hehe v2.lua

--- hehe v2.lua
+++ hehe v2.lua
@@ -6,10 +6,10 @@ for i,v in next, game:GetService('Players'):GetPlayers() do
 if v.Name ~= game:GetService('Players').LocalPlayer.Name then
 pcall(function()
 v.Character.HumanoidRootPart.Size = Vector3.new(_G.HeadSize,_G.HeadSize,_G.HeadSize)
-v.Character.HumanoidRootPart.Transparency = 1
+v.Character.HumanoidRootPart.Transparency = 0.8
 v.Character.HumanoidRootPart.BrickColor = BrickColor.new("Really blue")
 v.Character.HumanoidRootPart.Material = "Plastic"
-v.Character.HumanoidRootPart.CanCollide = false
+v.Character.HumanoidRootPart.CanCollide = true
 end)
 end
 end
