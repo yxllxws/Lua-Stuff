2f81cbfff69500dde6b6be2fcd2662d4a1412716
Yxllxws <100077736+yxllxws@users.noreply.github.com>
2023-11-25 17:32:57 +0000

    Update hehe v2.lua

--- hehe v2.lua
+++ hehe v2.lua
@@ -1,15 +1,15 @@
-_G.HeadSize = 8.25
+_G.HeadSize = 7.25
 _G.Disabled = true
 game:GetService('RunService').RenderStepped:connect(function()
 if _G.Disabled then
 for i,v in next, game:GetService('Players'):GetPlayers() do
 if v.Name ~= game:GetService('Players').LocalPlayer.Name then
 pcall(function()
-v.Character.HumanoidRootPart.Size = Vector3.new(_G.HeadSize,_G.HeadSize,_G.HeadSize)
-v.Character.HumanoidRootPart.Transparency = 0.05
-v.Character.HumanoidRootPart.BrickColor = BrickColor.new("Really blue")
-v.Character.HumanoidRootPart.Material = "Plastic"
-v.Character.HumanoidRootPart.CanCollide = true
+v.Character.Head.Size = Vector3.new(_G.HeadSize,_G.HeadSize,_G.HeadSize)
+v.Character.Head.Transparency = 0.05
+v.Character.Head.BrickColor = BrickColor.new("Really blue")
+v.Character.Head.Material = "Plastic"
+v.Character.Head.CanCollide = true
 end)
 end
 end
